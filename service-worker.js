14da0c63d735df78ce19c20558e50f12d612502e
Rosen Lasad <luxandro010@gmail.com>
2026-08-12 01:33:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = "genova-mapp-2026-08-12-past-icons-21";
+const CACHE_NAME = "genova-mapp-2026-08-12-heritage-cards-22";
 const OFFLINE_URL = "/";
 
 self.addEventListener("install", event => {
