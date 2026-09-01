15b4b243428a0f89609da9f0678b449889f493a4
Rosen Lasad <luxandro010@gmail.com>
2026-09-02 01:13:47 +0200

    Add files via upload

--- js/map-instance.js
+++ js/map-instance.js
@@ -1,5 +1,8 @@
 (function(){
   'use strict';
+  // Evita che l'elemento HTML con id="map" venga scambiato per l'istanza Leaflet
+  // dai moduli caricati prima dell'evento window.load.
+  if(!window.map || typeof window.map.eachLayer !== 'function') window.map=null;
   if(!window.L || typeof window.L.map !== 'function' || window.L.map.__gmCapturesInstance) return;
   var createMap=window.L.map;
   function capturedMap(){
